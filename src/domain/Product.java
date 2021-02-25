d0a90f4c6c3a39ff8b2acab7676f51222c8ee3bf
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2021-02-25 17:05:27 +0800

    create java classes, & setup JPA & CLI
    connectionName: classicmodelsPool

---
+++ src/domain/Product.java
@@ -0,0 +1,106 @@
+package domain;
+
+public class Product {
+	private String productCode;
+	private String productNmae;
+	private String productLine;
+	private String productScale;
+	private String productVendor;
+	private String productDescription;
+	private int quantityInStock;
+	private double buyPrice;
+	private double MSRP;
+	
+	public Product() {
+		
+	}
+	
+	public Product(String productCode, String productNmae, String productLine, String productScale,
+			String productVendor, String productDescription, int quantityInStock, double buyPrice, double mSRP) {
+		super();
+		this.productCode = productCode;
+		this.productNmae = productNmae;
+		this.productLine = productLine;
+		this.productScale = productScale;
+		this.productVendor = productVendor;
+		this.productDescription = productDescription;
+		this.quantityInStock = quantityInStock;
+		this.buyPrice = buyPrice;
+		MSRP = mSRP;
+	}
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+
+	public String getProductNmae() {
+		return productNmae;
+	}
+
+	public void setProductNmae(String productNmae) {
+		this.productNmae = productNmae;
+	}
+
+	public String getProductLine() {
+		return productLine;
+	}
+
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+
+	public String getProductScale() {
+		return productScale;
+	}
+
+	public void setProductScale(String productScale) {
+		this.productScale = productScale;
+	}
+
+	public String getProductVendor() {
+		return productVendor;
+	}
+
+	public void setProductVendor(String productVendor) {
+		this.productVendor = productVendor;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public int getQuantityInStock() {
+		return quantityInStock;
+	}
+
+	public void setQuantityInStock(int quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+
+	public double getBuyPrice() {
+		return buyPrice;
+	}
+
+	public void setBuyPrice(double buyPrice) {
+		this.buyPrice = buyPrice;
+	}
+
+	public double getMSRP() {
+		return MSRP;
+	}
+
+	public void setMSRP(double mSRP) {
+		MSRP = mSRP;
+	}
+	
+	
+	
+}
---
+++ src/domain/ProductLines.java
@@ -0,0 +1,56 @@
+package domain;
+
+public class ProductLines {
+	private String productLine;
+	private String textDescription;
+	private String htmlDescription;
+	private byte[] image;
+	
+	
+	
+	public ProductLines() {
+	}
+
+	public ProductLines(String productLine, String textDescription, String htmlDescription, byte[] image) {
+		super();
+		this.productLine = productLine;
+		this.textDescription = textDescription;
+		this.htmlDescription = htmlDescription;
+		this.image = image;
+	}
+
+	public String getProductLine() {
+		return productLine;
+	}
+
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+
+	public String getTextDescription() {
+		return textDescription;
+	}
+
+	public void setTextDescription(String textDescription) {
+		this.textDescription = textDescription;
+	}
+
+	public String getHtmlDescription() {
+		return htmlDescription;
+	}
+
+	public void setHtmlDescription(String htmlDescription) {
+		this.htmlDescription = htmlDescription;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	
+	
+	
+}
