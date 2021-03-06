726e9a82542baaf26403443068a43b368307bb2d
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2021-03-06 21:06:09 +0800

    check for qty input & quantityInStock available

--- src/domain/Cart.java
+++ src/domain/Cart.java
@@ -4,19 +4,17 @@ public class Cart {
 	private String productCode;
 	private int customerNumber;
 	private String requiredDate;
-	private String comments;
 	private int qty;
-	
+
 	public Cart() {
-		
+
 	}
-	
-	public Cart(String productCode, int customerNumber, String requiredDate, String comments, int qty) {
+
+	public Cart(String productCode, int customerNumber, String requiredDate, int qty) {
 		super();
 		this.productCode = productCode;
 		this.customerNumber = customerNumber;
 		this.requiredDate = requiredDate;
-		this.comments = comments;
 		this.qty = qty;
 	}
 
@@ -44,20 +42,12 @@ public class Cart {
 		this.requiredDate = requiredDate;
 	}
 
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-
 	public int getQty() {
 		return qty;
 	}
 
 	public void setQty(int qty) {
 		this.qty = qty;
-	}	
+	}
 	
 }
